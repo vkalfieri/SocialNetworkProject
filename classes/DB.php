cc4502ccd576091f99c21d12df31b05a3ea3c776
vkalfieri <phantom_11@hotmail.com>
2017-10-18 14:56:28 +1100

    video 5 of social network tutorial howcode youtube
    completed login with cookies, and token creation, up to video 6 and completed video 5.

--- classes/DB.php
+++ classes/DB.php
@@ -1,7 +1,7 @@
 <?php
 class DB {
     private static function connect(){
-        $pdo = new PDO('mysql:host=127.0.0.1;dbname=SocialNetwork;charset=utf8', 'root', 'jinkies81');
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=socialnetwork;charset=utf8', 'root');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
@@ -9,7 +9,11 @@ class DB {
     public static function query($query, $params = array()) {
         $statement = self::connect()->prepare($query);
         $statement->execute($params);
-        //$data = $statement->fetchAll();
-        //return $data;
+
+        if (explode(' ', $query)[0] == 'SELECT') {
+        $data = $statement->fetchAll();
+        return $data;
     }
+}
+
 }
