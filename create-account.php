4959fa7d607d321019af63ebadcad28db1a3b8a9
vkalfieri <phantom_11@hotmail.com>
2017-10-18 17:19:52 +1100

    added token functionality
    part 6 of how code social network project

--- create-account.php
+++ create-account.php
@@ -9,9 +9,13 @@ if (isset($_POST['createaccount'])) {
                         if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
                                 if (strlen($password) >= 6 && strlen($password) <= 60) {
                                 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                                if (!DB::query('SELECT email FROM users WHERE email=:email', array(':email'=>$email))) {
                                         DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email)', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
                                         echo "Success!";
                                 } else {
+                                        echo 'Email in use!';
+                                }
+                        } else {
                                         echo 'Invalid email!';
                                 }
                         } else {
